72917a1a7c51794f5ff32d75e2f63d4563763000
Flunnys <103204990+Flunnys@users.noreply.github.com>
2022-11-07 14:07:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let blockCard = false;
 let firstCard, secondCard;
+
+
+function makeCounter() {
+    let counter = 0;
+      for (let i=0; i<cards.length; i++) {
+        let btn = cards[i];
+        btn.onclick = function() {
+          counter ++;
+          btn.innerHTML = counter;
+        }
+      }
+  }
 cards.forEach(card => card.addEventListener('click', flipCard));
